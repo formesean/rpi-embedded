4b8af342199abff19828ca82bcd8abbd37e20f6f
formesean <seanaguilar698@gmail.com>
2025-02-23 14:13:24 +0800

    rename zero w to zero-w

---
+++ zero-w/compile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check if a filename was provided
+if [ -z "$1" ]; then
+  echo "Usage: ./compile.sh <main_file.cpp>"
+  exit 1
+fi
+
+# Extract the main file name without extension
+MAIN_FILE=$1
+OUTPUT_NAME=${MAIN_FILE%.cpp}
+
+# Compile the file
+g++ -o "$OUTPUT_NAME" "$MAIN_FILE" ../lib/MCP23S17.cpp -lpigpio -lrt -pthread
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+  echo "Compilation successful. Run ./$OUTPUT_NAME"
+else
+  echo "Compilation failed."
+fi
